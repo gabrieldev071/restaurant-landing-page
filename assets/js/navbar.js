257012e2f924361feed512416a4b7255c2ec38ac
Gabriel Souza <106005629+gabrieldev071@users.noreply.github.com>
2025-01-08 23:55:42 -0300

    update nav.bar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,7 +1,44 @@
-$(document).ready(function () {
-    $('.header__toggle').on('click', function () {
-        $('.header__menu').toggleClass('active');
-        $('.btn-laranja').toggleClass('active');
-        $(this).find('i').toggleClass('fa-x');
-    });
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation = link.style.animation
+                ? ""
+                : `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const mobileNavbar = new MobileNavbar(
+        ".mobile-menu",
+        ".header__menu",
+        ".header__menu-item"
+    );
+    mobileNavbar.init();
 });
